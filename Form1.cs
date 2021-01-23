6a1aa3c308ae0a6417482054b660d32a761dced8
Каргин Сергей <azovolimp2017@yandex.ru>
2021-01-23 21:10:34 +0300

    fffff

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace WindowsFormsApp167
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dddddd
+        }
     }
 }
